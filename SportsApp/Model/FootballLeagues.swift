3422b0f0bb578e1081629962756b1201754a4ddf
Tasneem Ibraheem <53406540+tasneem-ops@users.noreply.github.com>
2024-05-12 18:01:29 +0300

    Revert "Development"

--- SportsApp/Model/FootballLeagues.swift
+++ SportsApp/Model/FootballLeagues.swift
@@ -1,22 +1,25 @@
 //
-//  BasketballLeagues.swift
+//  FootballLeagues.swift
 //  SportsApp
 //
-//  Created by JETSMobileLabMini9 on 12/05/2024.
+//  Created by Tasnem on 11/05/2024.
 //
 
 import Foundation
-
-struct SportLeagues: Codable {
+struct FootballLeagues: Codable {
 
   var leagueKey   : Int?    = nil
   var leagueName  : String? = nil
-
+  var countryKey  : Int?    = nil
+  var countryName : String? = nil
 
   enum CodingKeys: String, CodingKey {
 
     case leagueKey   = "league_key"
     case leagueName  = "league_name"
+    case countryKey  = "country_key"
+    case countryName = "country_name"
+  
   }
 
   init(from decoder: Decoder) throws {
@@ -24,6 +27,9 @@ struct SportLeagues: Codable {
 
     leagueKey   = try values.decodeIfPresent(Int.self    , forKey: .leagueKey   )
     leagueName  = try values.decodeIfPresent(String.self , forKey: .leagueName  )
+    countryKey  = try values.decodeIfPresent(Int.self    , forKey: .countryKey  )
+    countryName = try values.decodeIfPresent(String.self , forKey: .countryName )
+ 
   }
 
   init() {
